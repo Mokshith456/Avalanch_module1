dd636188fa545eec352a89a40f5805eb4e9fc566
Mokshith456 <110161392+Mokshith456@users.noreply.github.com>
2023-11-11 13:46:20 +0530

    Update errorhandeling.sol

--- errorhandeling.sol
+++ errorhandeling.sol
@@ -1,21 +1,42 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
-contract Error_handling {
-    function require_method(uint256 x, uint256 y) public pure returns (uint256) {
-        require((x != 0) && (y != 0) && (x % y == 0), "X must not be zero and divisible by Y");
-        return x / y;
+//import "hardhat/console.sol";
+
+contract Assessment {
+
+    uint256 public balance;
+
+
+    constructor(uint initBalance) payable {
+        balance = initBalance;
     }
 
-    function assert_method(uint256 p, uint256 q) public  pure returns (uint256) {
-        assert(p > 0 && q > 0 && p * q > p + q);
-        return p * q;
+    function getBalance() public view returns(uint256){
+        return balance;
     }
 
-    function revert_method(uint256 value) public pure returns (string memory) {
-        if (value < 100 || value > 999) {
-            revert("Value must be between 100 and 999");
+    function array_sum(uint[] memory array) public payable {
+
+        // perform transaction
+        assert(array.length>0);
+        uint256 result  = 0;
+        for(uint i=0 ; i<array.length ; i++){
+            result += array[i];
         }
-        return "Value is a three digit number, you can go ahead with the transaction";
+        balance  = result;
+    }
+
+    // custom error
+
+    function array_product(uint[] memory array) public {
+ 
+        assert(array.length>0);
+        uint256 result = 1;
+        for(uint i=0 ; i<array.length ; i++){
+            result *= array[i];
+        }
+        balance = result ;
+
     }
 }
